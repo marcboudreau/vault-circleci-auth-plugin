14aa878519ed364f32a328c700ad7e5a89a4e592
Marc Boudreau <marc.boudreau@qlik.com>
2018-01-28 22:08:11 -0500

    Adding project policy map

--- backend.go
+++ backend.go
@@ -7,17 +7,25 @@ import (
 
 type backend struct {
 	*framework.Backend
+
+	ProjectMap *framework.PolicyMap
 }
 
 // Backend creates a new backend with the provided BackendConfig.
 func Backend(c *logical.BackendConfig) *backend {
 	var b backend
 
+	b.ProjectMap = &framework.PolicyMap{
+		PathMap: framework.PathMap{
+			Name: "projects",
+		},
+		DefaultKey: "default",
+	}
+
+	allPaths := append(b.ProjectMap.Paths(), pathConfig(&b))
 	b.Backend = &framework.Backend{
 		BackendType: logical.TypeCredential,
-		Paths: []*framework.Path{
-			pathConfig(&b),
-		},
+		Paths: allPaths,
 	}
 
 	b.Backend.Setup(c)
